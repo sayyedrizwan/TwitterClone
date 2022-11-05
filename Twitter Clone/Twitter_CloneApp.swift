ba0a5aedb58b725d978531251d7a57d77f1e75a7
Rizwan Sayyed <sayyedrizwanahmed@gmail.com>
2022-11-05 12:47:36 +0530

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by Sayyed Rizwan on 05/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
